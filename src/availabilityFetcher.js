3844d607af0137f9dd922607d10777b9bebf6dc2
jdfandino10 <jd.fandino10@uniandes.edu.co>
2018-07-18 16:33:29 -0500

    added hability to show availability when a new course is added/removed

--- src/availabilityFetcher.js
+++ src/availabilityFetcher.js
@@ -5,16 +5,17 @@ let mustReUpdate = false;
 let availabilityInterval = null;
 
 // Inserts availability column to the DOM and returns the existing courses
-function insertAvailabilityColumn() {
-	let courses = [];
+function insertAvailabilityColumn(courses) {
 	// Find courses table
 	const table = $('#summaryInfo table');
 
 	// Add "Cupos" header
-	table.find('thead tr').prepend('<th scope="col" data-sort-direction="disabled"' +
-      ' class="sort-disabled availability-col ui-state-default" data-property="availability"' +
-      ' xe-field="availability" style="width: 8em;"><div class="title" title="Cupos" style="width:' +
-      ' auto;">Cupos</div></th>');
+  if ($('div[title=Cupos]').length == 0) {
+  	table.find('thead tr').prepend('<th scope="col" data-sort-direction="disabled"' +
+        ' class="sort-disabled availability-col ui-state-default" data-property="availability"' +
+        ' xe-field="availability" style="width: 8em;"><div class="title" title="Cupos" style="width:' +
+        ' auto;">Cupos</div></th>');
+  }
 
 	// Add availability column with id
 	table.find('tbody tr').each((index, elem) => {
@@ -24,11 +25,15 @@ function insertAvailabilityColumn() {
 		const courseCode = elem.find('td[data-property=subjectCourseSectionNumber]')
         .text().split(',')[0].replace(/ /g,'').toUpperCase();
 		const availabilityId = courseCRN + 'availability';
-		courses.push({courseCRN, courseCode, availabilityId});
+    let currCourse = courses.find((course) => course.courseCRN == courseCRN);
+		if (!currCourse) {
+      currCourse = {courseCRN, courseCode, availabilityId, availability: '-'};
+      courses.push(currCourse);
+    }
 		if ($("#" + availabilityId).length > 0) return;
     elem.prepend('<td id="' + availabilityId + '"data-id="' + currId +
         '" data-property="availability" xe-field="availability" class="readonly"' +
-        ' style="width: auto;"> - </td>');
+        ' style="width: auto;">' + currCourse.availability + '</td>');
   });
 
   // Return the inserted courses
@@ -69,6 +74,8 @@ function updateAllCoursesHelper(courses, index) {
         console.log('Updated: ' + courseCode);
         // Update availability if the course was found
         $("#" + availabilityId).text(course.seatsAvailable);
+        // Save a cache in case the table gets restored
+        courses[index].availability = course.seatsAvailable;
       }
       // continue with the next course
       // Note: Calls can't be done async because reset call might get messed up,
@@ -81,7 +88,8 @@ function updateAllCoursesHelper(courses, index) {
 }
 
 function main() {
-	const courses = insertAvailabilityColumn();
+  let courses = [];
+	insertAvailabilityColumn(courses);
 	updateAllCourses(courses);
 	// Update on intervals of 30 seconds (if too low, requests may get blocked)
 	availabilityInterval = setInterval(() => {
@@ -91,6 +99,19 @@ function main() {
 			updateAllCoursesHelper(courses, 0);
 		}
 	}, 30 * 1000);
+
+  $("#saveButton").click(() => {
+    let modifiedCoursesInterval = setInterval(() => {
+      console.log('Waiting to get saved...');
+      if ($("div.loading").length == 0) {
+        courses = insertAvailabilityColumn(courses);
+        console.log('courses updated!');
+        clearInterval(modifiedCoursesInterval);
+      }
+    }, 2.5 * 1000);
+  });
+
 }
+
 
 main();
