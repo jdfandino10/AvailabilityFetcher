bf8dafb6733d5976242e9c8705c2507e074f452c
jdfandino10 <jd.fandino10@uniandes.edu.co>
2018-07-18 14:27:08 -0500

    Initial working script

---
+++ availabilityFetcher.js
@@ -0,0 +1,96 @@
+// Variable to know if interval must re update
+let mustReUpdate = false;
+
+// Variable in case user wants to clearInterval(availabilityInterval)
+let availabilityInterval = null;
+
+// Inserts availability column to the DOM and returns the existing courses
+function insertAvailabilityColumn() {
+	let courses = [];
+	// Find courses table
+	const table = $('#summaryInfo table');
+
+	// Add "Cupos" header
+	table.find('thead tr').prepend('<th scope="col" data-sort-direction="disabled"' +
+      ' class="sort-disabled availability-col ui-state-default" data-property="availability"' +
+      ' xe-field="availability" style="width: 8em;"><div class="title" title="Cupos" style="width:' +
+      ' auto;">Cupos</div></th>');
+
+	// Add availability column with id
+	table.find('tbody tr').each((index, elem) => {
+		elem = $(elem);
+		const currId = elem.attr('data-id');
+		const courseCRN = elem.find('td[data-property=courseReferenceNumber]').text();
+		const courseCode = elem.find('td[data-property=subjectCourseSectionNumber]')
+        .text().split(',')[0].replace(/ /g,'').toUpperCase();
+		const availabilityId = courseCRN + 'availability';
+		courses.push({courseCRN, courseCode, availabilityId});
+		if ($("#" + availabilityId).length > 0) return;
+    elem.prepend('<td id="' + availabilityId + '"data-id="' + currId +
+        '" data-property="availability" xe-field="availability" class="readonly"' +
+        ' style="width: auto;"> - </td>');
+  });
+
+  // Return the inserted courses
+  return courses;
+}
+
+// Function that updates all courses
+function updateAllCourses(courses) {
+	// Go on each course
+	updateAllCoursesHelper(courses, 0);
+}
+
+// Calls the api and updates the availability UI for all courses recursively.
+function updateAllCoursesHelper(courses, index) {
+  if (index >= courses.length) {
+    // If got all, mustReUpdate on next interval
+    mustReUpdate = true;
+    console.log('Done updating all courses');
+    console.log('-------------------------------');
+    return;
+  }
+  mustReUpdate = false;
+  const courseCode = courses[index].courseCode;
+  const courseCRN = courses[index].courseCRN;
+  const availabilityId = courses[index].availabilityId;
+  const resetUrl = '/StudentRegistrationSsb/ssb/classSearch/resetDataForm';
+  const sectionsUrl = '/StudentRegistrationSsb/ssb/searchResults/searchResults?' +
+      'txt_subjectcoursecombo=' + courseCode + '&txt_term=201820&pageOffset=0&page' +
+      'MaxSize=50&sortColumn=subjectDescription&sortDirection=asc';
+  
+  // Resets last query (api requires this call so next data is accurate)
+  $.get(resetUrl).done(() => {
+    // Gets all sections of course with code courseCode
+    $.get(sectionsUrl).done((data) => {
+      // Filter the specific course from all the courses according to the courseCRN
+      const course = data.data.filter((course) => course.courseReferenceNumber == courseCRN)[0];
+      if (course) {
+        console.log('Updated: ' + courseCode);
+        // Update availability if the course was found
+        $("#" + availabilityId).text(course.seatsAvailable);
+      }
+      // continue with the next course
+      // Note: Calls can't be done async because reset call might get messed up,
+      // thats why we continue once we're done with the current course.
+      updateAllCoursesHelper(courses, index + 1);
+    }).fail((data) => {
+      console.log("Error fetching availability of " + courseCode + " - " + courseCRN);
+    });
+  });
+}
+
+function main() {
+	const courses = insertAvailabilityColumn();
+	updateAllCourses(courses);
+	// Update on intervals of 30 seconds (if too low, requests may get blocked)
+	availabilityInterval = setInterval(() => {
+		if (mustReUpdate) {
+      console.log('-------------------------------');
+      console.log('Starting update');
+			updateAllCoursesHelper(courses, 0);
+		}
+	}, 30 * 1000);
+}
+
+main();
